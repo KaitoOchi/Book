a7a98899f4c45ad9c48df85201ae1eeff633a03e
NoraNo <CA01972044@st.kawahara.ac.jp>
2023-03-08 16:41:43 +0900

    aro-

--- Book/Game/Player3D.cpp
+++ Book/Game/Player3D.cpp
@@ -27,13 +27,13 @@ bool Player3D::Start()
 	m_animationClips[m_enAnimationClip_Throw].Load("Assets/animData/player/use2.tka");
 	m_animationClips[m_enAnimationClip_Throw].SetLoopFlag(false);
 	//モデルを読み込む
-	//m_modelRender.Init("Assets/modelData/player/player.tkm",m_animationClips,m_enAnimationClip_Num);
+	m_modelRender->Init("Assets/modelData/player/player.tkm",m_animationClips,m_enAnimationClip_Num);
 	//キャラコンを初期化する。
 	/*m_3Dcharacon.Init(25.0f, 75.0f, m_position);*/
-	m_modelRender.SetPosition(m_position);
-	m_modelRender.SetRotation(Quaternion::Identity);
-	m_modelRender.SetScale(Vector3::One);
-	m_modelRender.Update();
+	m_modelRender->SetPosition(m_position);
+	m_modelRender->SetRotation(Quaternion::Identity);
+	m_modelRender->SetScale(Vector3::One);
+	m_modelRender->Update();
 	return true;
 }
 
@@ -41,16 +41,17 @@ void Player3D::Update()
 {
 	Player::Update();
 	Animation();
-	if (g_pad[0]->IsTrigger(enButtonRB1))
+
+	if (g_pad[0]->IsTrigger(enButtonRB1)&&fabsf(m_moveSpeed.y)<0.001)
 	{
 		Throw();
 	}
 	//プレイヤーの移動を継承する。
 	//キャラコンで座標を移動させる。
 	m_position = m_characon.Execute(m_moveSpeed, g_gameTime->GetFrameDeltaTime());
-	m_modelRender.SetPosition(m_position);
-	m_modelRender.SetRotation(m_rotation);
-	m_modelRender.Update();
+	m_modelRender->SetPosition(m_position);
+	m_modelRender->SetRotation(m_rotation);
+	m_modelRender->Update();
 
 }
 void Player3D::Throw()
@@ -64,23 +65,23 @@ void Player3D::Animation()
 	switch (m_playerState)
 	{
 	case Player::m_enPlayer_Idle:
-		m_modelRender.PlayAnimation(m_enAnimationClip_Idle, 0.5f);
+		m_modelRender->PlayAnimation(m_enAnimationClip_Idle, 0.5f);
 		break;
 	case Player::m_enPlayer_walk:
-		m_modelRender.PlayAnimation(m_enAnimationClip_Walk, 0.1f);
+		m_modelRender->PlayAnimation(m_enAnimationClip_Walk, 0.1f);
 		break;
 	case Player::m_enPlayer_Run:
-		m_modelRender.PlayAnimation(m_enAnimationClip_Run, 0.1f);
+		m_modelRender->PlayAnimation(m_enAnimationClip_Run, 0.1f);
 		break;
 	case Player::m_enPlayer_Jump:
-		m_modelRender.PlayAnimation(m_enAnimationClip_Jump, 0.5f);
+		m_modelRender->PlayAnimation(m_enAnimationClip_Jump, 0.0f);
 		break;
 	case Player::m_enPlayer_Jumpend:
 		break;
 	case Player::m_enPlayer_Change:
 		break;
 	case Player::m_enPlayer3D_Throw:
-		m_modelRender.PlayAnimation(m_enAnimationClip_Throw, 0.5f);
+		m_modelRender->PlayAnimation(m_enAnimationClip_Throw, 0.1f);
 		break;
 	default:
 		break;
@@ -88,5 +89,5 @@ void Player3D::Animation()
 }
 void Player3D::Render(RenderContext& rc)
 {
-	m_modelRender.Draw(rc);
+	m_modelRender->Draw(rc);
 }
