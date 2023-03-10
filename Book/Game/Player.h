0c01696dfc11b194f62198f5e272d2c1ee2d0d96
nom0531 <norrrrrra0504@gmail.com>
2023-03-10 10:02:07 +0900

    Revert "Merge branch 'camera' of github.com:KaitoOchi/Book"
    This reverts commit 4e1d357966d9c0ccce40b5a6be196e65b153de45, reversing
    changes made to e1653cc5f6b886d1511e0fae82f949abc2c49439.

--- Book/Game/Player.h
+++ Book/Game/Player.h
@@ -11,19 +11,10 @@ public:
 		m_enPlayer_Jump,//ジャンプ
 		m_enPlayer_Jumpend,//ジャンプ終わり
 		m_enPlayer_Change,//切替
-		m_enPlayer3D_Throw,//投げる
-		m_enPlayer3D_Steal,//盗む
-		m_enPlayer3D_Push,//押す
 	};
 	Player();
 	~Player();
 	bool Start();
-	//座標の取得
-	const Vector3& GetPosition()const
-	{
-		return m_position;
-	}
-	
 protected:
 	void Update();
 	void Move();
@@ -61,30 +52,17 @@ protected:
 	/// 変更ステートの遷移処理
 	/// </summary>
 	void ProcessChangeStateTransition();
-	/// <summary>
-	/// 投げるステートの遷移処理
-	/// </summary>
-	void ProcessThrowStateTransition();
-	/// <summary>
-	/// 盗むステートの遷移処理
-	/// </summary>
-	void ProcessStealStateTransition();
-	/// <summary>
-	/// 押すステートの遷移処理
-	/// </summary>
-	void ProcessPushStateTransition();
 	
-	
-
 	Vector3 m_moveSpeed=Vector3::Zero;//移動速度
 	Vector3 m_Lstic = Vector3::Zero;//左ステック
 	Vector3 m_position = Vector3(0.0f, 0.0f, 0.0f);//初期座標
 	Vector3 m_forward = Vector3::AxisZ;//プレイヤーの正面ベクトル
 	
-	
+	float m_walk = 20.0f;//歩き時の乗算量
+	float m_run = 40.0f;//走り時の乗算量
+	float m_jumpvolume = 150.0f;//ジャンプ量
 	float angle=0;//回転角度
 	
-	ModelRender *m_modelRender=nullptr;//モデル
 	Quaternion m_rotation;//回転
 	CharacterController m_characon;//キャラコン
 	EnPlayerState m_playerState = m_enPlayer_Idle;//待機状態
--- Book/Game/Player3D.cpp
+++ Book/Game/Player3D.cpp
@@ -10,7 +10,6 @@ Player3D::~Player3D()
 }
 bool Player3D::Start()
 {
-	m_modelRender = new ModelRender;
 	Player::Start();
 	//アニメーションを読み込む
 	m_animationClips[m_enAnimationClip_Idle].Load("Assets/animData/player/idle.tka");
@@ -28,13 +27,13 @@ bool Player3D::Start()
 	m_animationClips[m_enAnimationClip_Throw].Load("Assets/animData/player/use2.tka");
 	m_animationClips[m_enAnimationClip_Throw].SetLoopFlag(false);
 	//モデルを読み込む
-	m_modelRender->Init("Assets/modelData/player/player.tkm",m_animationClips,m_enAnimationClip_Num);
+	m_model3DRender.Init("Assets/modelData/player/player.tkm",m_animationClips,m_enAnimationClip_Num);
 	//キャラコンを初期化する。
 	/*m_3Dcharacon.Init(25.0f, 75.0f, m_position);*/
-	m_modelRender->SetPosition(m_position);
-	m_modelRender->SetRotation(Quaternion::Identity);
-	m_modelRender->SetScale(Vector3::One);
-	m_modelRender->Update();
+	m_model3DRender.SetPosition(m_position);
+	m_model3DRender.SetRotation(Quaternion::Identity);
+	m_model3DRender.SetScale(Vector3::One);
+	m_model3DRender.Update();
 	return true;
 }
 
@@ -42,46 +41,40 @@ void Player3D::Update()
 {
 	Player::Update();
 	Animation();
-	if (g_pad[0]->IsTrigger(enButtonRB1))
-	{
-		Throw();
-	}
+	Throw();
 	//プレイヤーの移動を継承する。
 	//キャラコンで座標を移動させる。
 	m_position = m_characon.Execute(m_moveSpeed, g_gameTime->GetFrameDeltaTime());
-	m_modelRender->SetPosition(m_position);
-	m_modelRender->SetRotation(m_rotation);
-	m_modelRender->Update();
+	m_model3DRender.SetPosition(m_position);
+	m_characon.SetPosition(m_position);
+	m_model3DRender.SetRotation(m_rotation);
+	m_model3DRender.Update();
 
 }
 void Player3D::Throw()
 {
-	m_playerState = m_enPlayer3D_Throw;
-}
 
+}
 void Player3D::Animation()
 {
 	//プレイヤーのステートによって変える
 	switch (m_playerState)
 	{
 	case Player::m_enPlayer_Idle:
-		m_modelRender->PlayAnimation(m_enAnimationClip_Idle, 0.5f);
+		m_model3DRender.PlayAnimation(m_enAnimationClip_Idle, 0.5f);
 		break;
 	case Player::m_enPlayer_walk:
-		m_modelRender->PlayAnimation(m_enAnimationClip_Walk, 0.1f);
+		m_model3DRender.PlayAnimation(m_enAnimationClip_Walk, 0.1f);
 		break;
 	case Player::m_enPlayer_Run:
-		m_modelRender->PlayAnimation(m_enAnimationClip_Run, 0.1f);
+		m_model3DRender.PlayAnimation(m_enAnimationClip_Run, 0.1f);
 		break;
 	case Player::m_enPlayer_Jump:
-		m_modelRender->PlayAnimation(m_enAnimationClip_Jump, 0.8f);
+		m_model3DRender.PlayAnimation(m_enAnimationClip_Jump, 0.5f);
 		break;
 	case Player::m_enPlayer_Jumpend:
 		break;
 	case Player::m_enPlayer_Change:
-		break;
-	case Player::m_enPlayer3D_Throw:
-		m_modelRender->PlayAnimation(m_enAnimationClip_Throw, 0.5f);
 		break;
 	default:
 		break;
@@ -89,5 +82,5 @@ void Player3D::Animation()
 }
 void Player3D::Render(RenderContext& rc)
 {
-	m_modelRender->Draw(rc);
+	m_model3DRender.Draw(rc);
 }
