49fac739a54345f05fd16409e245d77344c5eb8a
nom0531 <norrrrrra0504@gmail.com>
2023-03-13 17:53:09 +0900

    Enemyの処理を追加

--- Book/Game/Enemy.cpp
+++ Book/Game/Enemy.cpp
@@ -1,9 +1,18 @@
 #include "stdafx.h"
 #include "Enemy.h"
 
-#include "Player.h"
+#include "Player2D.h"
+#include "Player3D.h"
 
-#define CATCHDECISION 10.0f	// 確保した判定になる範囲
+#define FIELDOFVIEW Math::PI / 180.0f) * 120.0f // エネミーの視野角(初期値120)
+
+namespace
+{
+	const float		 LINEARCOMPLETION = 0.5f;						// 線形補完のフレーム数
+	const float		 CATCHDECISION = 10.0f;					// プレイヤーを確保したことになる範囲
+	const float		 SCALESIZE = 1.3f;						// SetScaleのサイズ
+	const Vector3	 BOXSIZE = { 75.0f, 90.0f,60.0f };	// CharacterControllerのサイズ
+}
 
 Enemy::Enemy()
 {
@@ -18,19 +27,26 @@ bool Enemy::Start()
 {
 	// モデルの読み込み
 	m_modelRender.Init("Assets/modelData/enemy/enemy.tkm");
-	m_modelRender.SetPosition({ 0.0f,0.0f,0.0f });
+	m_modelRender.SetScale({ 1.3f, 1.3f, 1.3f });
 
 	// アニメーションの読み込み
-	//m_animationClips[enEnemyAnimationClip_Idle].Load("Assets/animData/enemy/idle.tka");
-	//m_animationClips[enEnemyAnimationClip_Idle].SetLoopFlag(true);
-	//m_animationClips[enEnemyAnimationClip_Walk].Load("Assets/animData/enemy/walk.tka");
-	//m_animationClips[enEnemyAnimationClip_Walk].SetLoopFlag(true);
-	//m_animationClips[enEnemyAnimationClip_Run].Load("Assets/animData/enemy/run.tka");
-	//m_animationClips[enEnemyAnimationClip_Run].SetLoopFlag(true);
-	//m_animationClips[enEnemyAnimationClip_Attack].Load("Assets/animData/enemy/attack.tka");
-	//m_animationClips[enEnemyAnimationClip_Attack].SetLoopFlag(false);
-	//m_animationClips[enEnemyAnimationClip_Damege].Load("Assets/animData/enemy/damege.tka");
-	//m_animationClips[enEnemyAnimationClip_Damege].SetLoopFlag(false);
+	m_animationClips[m_enAnimationClip_Idle].Load("Assets/animData/enemy/idle.tka");
+	m_animationClips[m_enAnimationClip_Idle].SetLoopFlag(true);
+	m_animationClips[m_enAnimationClip_Walk].Load("Assets/animData/enemy/walk.tka");
+	m_animationClips[m_enAnimationClip_Walk].SetLoopFlag(true);
+	m_animationClips[m_enAnimationClip_Run].Load("Assets/animData/enemy/run.tka");
+	m_animationClips[m_enAnimationClip_Run].SetLoopFlag(true);
+	m_animationClips[m_enAnimationClip_Attack].Load("Assets/animData/enemy/attack1.tka");
+	m_animationClips[m_enAnimationClip_Attack].SetLoopFlag(false);
+	m_animationClips[m_enAnimationClip_Damege].Load("Assets/animData/enemy/damege.tka");
+	m_animationClips[m_enAnimationClip_Damege].SetLoopFlag(false);
+
+	// キャラクターコントローラーを初期化する
+	m_characterController.Init(BOXSIZE, m_position);
+
+	// プレイヤーのインスタンスを探す
+	m_player2D = FindGO<Player2D>("player2d");
+	m_player3D = FindGO<Player3D>("player3d");
 
 	return true;
 }
@@ -38,35 +54,74 @@ bool Enemy::Start()
 void Enemy::Update()
 {
 	// 更新
-	//m_modelRender.PlayAnimation(enEnemyAnimationClip_Idle);
+	//HeadToDestination();
 	m_modelRender.Update();
-}
-
-void Enemy::MoveState()
-{
-	// エネミーの行動処理
 }
 
 void Enemy::HeadToDestination()
 {
-	// 目的地に向かう処理
 	// プレイヤーを見失ったとき
 	if (MissigPlayerFlag) {
+		// 巡回に移行する
+		m_enEnemyActState = m_enEnemyActState_Craw;
+
 		// 直近のパスを探す
 		// パスに戻ったときにfalseにする
 	}
+
 	// プレイヤーを発見したとき
 	if (FoundPlayerFlag) {
+		// 追跡に移行する
+		m_enEnemyActState = m_enEnemyActState_Tracking;
+
 		// プレイヤーに向かう経路を作成
-	}	
+	}
+
+	// 目的地に向かう処理
+	Act();
 }
 
-void Enemy::Seach()
+void Enemy::Act()
+{
+	// 行動パターン
+	switch (m_enEnemyActState) {
+		// 巡回
+	case Enemy::m_enEnemyActState_Craw:
+		break;
+		// 追跡
+	case Enemy::m_enEnemyActState_Tracking:
+		break;
+	}
+}
+
+bool Enemy::SeachPlayer()
 {
 	// 視野角の処理
+	// trueのときプレイヤーを発見している
+	bool flag = false;
+
+	// エネミーからプレイヤーへ向かうベクトル
+	Vector3 diff =  /*playerの座標*/ -m_position;
+
+	// プレイヤーにある程度近いとき
+	if (diff.LengthSq() <= 700.0 * 700.0f) {
+		// エネミーからプレイヤーへ向かうベクトルを正規化
+		diff.Normalize();
+		// エネミーの正面ベクトル、エネミーからプレイヤーへ向かうベクトルの内積を計算
+		float cos = m_forward.Dot(diff);
+		// 内積から角度を計算
+		float angle = acosf(cos);
+		// 角度が視野角より狭いとき
+		if (angle <= (FIELDOFVIEW) {
+			// プレイヤーを発見
+			flag = true;
+		}
+	}
+
+	return flag;
 }
 
-void Enemy::CatchPlayer()
+bool Enemy::CatchPlayer()
 {
 	// プレイヤーを確保する処理
 	// エネミーからプレイヤーへ向かうベクトルを計算する
@@ -88,7 +143,35 @@ void Enemy::HitFlashBullet()
 	// 閃光弾が当たったときの処理
 }
 
+void Enemy::Animation()
+{
+	// アニメーションのステート
+	switch (m_enEnemyAnimationState) {
+		// 待機
+	case Enemy::m_enEnemyAnimationState_Idle:
+		m_modelRender.PlayAnimation(m_enAnimationClip_Idle, LINEARCOMPLETION);
+		break;
+		// 歩く
+	case Enemy::m_enEnemyAnimationState_Walk:
+		m_modelRender.PlayAnimation(m_enAnimationClip_Walk, LINEARCOMPLETION);
+		break;
+		// 走る
+	case Enemy::m_enEnemyAnimationState_Run:
+		m_modelRender.PlayAnimation(m_enAnimationClip_Run, LINEARCOMPLETION);
+		break;
+		// 攻撃
+	case Enemy::m_enEnemyAnimationState_Attack:
+		m_modelRender.PlayAnimation(m_enAnimationClip_Attack, LINEARCOMPLETION);
+		break;
+		// 被弾
+	case Enemy::m_enEnemyAnimationState_Damege:
+		m_modelRender.PlayAnimation(m_enAnimationClip_Damege, LINEARCOMPLETION);
+		break;
+	}
+}
+
 void Enemy::Render(RenderContext& rc)
 {
+	// 描画
 	m_modelRender.Draw(rc);
 }
--- Book/Game/Enemy.h
+++ Book/Game/Enemy.h
@@ -1,5 +1,5 @@
 #pragma once
-class Enemy:public IGameObject
+class Enemy :public IGameObject
 {
 public:
 	Enemy();
@@ -7,45 +7,118 @@ public:
 
 	bool Start();
 	void Update();
-	void Render(RenderContext& rc);
 
-	void MoveState();					// 行動パターンの処理
-	void HeadToDestination();			// 目的地に向かう処理
-	void Seach();						// 視野角の処理
-	void CatchPlayer();					// プレイヤーを確保する処理
+	void MoveDestination();				// 目的地に移動する処理
+	void Search();						// エネミーの索敵範囲
 	void HitFlashBullet();				// 閃光弾が当たったときの処理
+	void CatchPlayer();					// プレイヤーを捕まえる処理
 
-	// 被弾フラグ設定用。必要なければ消去
-	// trueのとき被弾
-	void SetHitFlashBullet(bool b) {
-		// 引数を渡す
-		b = HitFlashBulletFlag;
+	void Render(RenderContext& rc);
+#pragma once
+
+	class Player3D;
+	class Player2D;
+	class Enemy :public IGameObject
+	{
+	public:
+		Enemy();
+		~Enemy();
+
+		bool Start();
+		void Update();
+		void Render(RenderContext& rc);
+
+		bool SeachPlayer();					// プレイヤーを発見する処理
+		bool CatchPlayer();					// プレイヤーを確保する処理
+		void HeadToDestination();			// 目的地に向かう処理
+		void HitFlashBullet();				// 閃光弾が当たったときの処理
+
+		void Act();							// 行動パターン
+		void Animation();					// アニメーション
+
+		/// <summary>
+		/// 被弾フラグ設定用。必要なければ消去
+		/// </summary>
+		/// <param name="">被弾したかどうかどうか判定する。trueなら被弾したと判定</param>
+		void SetHitFlashBullet(bool b) {
+			// 引数を渡す
+			b = HitFlashBulletFlag;
+		};
+
+	private:
+
+		Player2D* m_player2D;
+		Player3D* m_player3D;
+
+		CharacterController m_characterController;
+
+		ModelRender m_modelRender;			// モデルレンダー
+		FontRender m_fontRender;			// フォントレンダー
+
+		// アニメーションクリップ
+		enum EnEnemyAnimationClip
+		{
+			m_enAnimationClip_Idle,			// 待機
+			m_enAnimationClip_Walk,			// 歩く
+			m_enAnimationClip_Run,			// 走る
+			m_enAnimationClip_Attack,		// 攻撃
+			m_enAnimationClip_Damege,		// 被弾
+			m_enAnimationClip_Num
+		};
+		// アニメーションクリップ
+		AnimationClip m_animationClips[m_enAnimationClip_Num];
+
+
+		// エネミーのアニメーションステート
+		enum EnEnemyAnimationState
+		{
+			m_enEnemyAnimationState_Idle,	// 待機
+			m_enEnemyAnimationState_Walk,	// 歩く
+			m_enEnemyAnimationState_Run,	// 走る
+			m_enEnemyAnimationState_Attack,	// 攻撃
+			m_enEnemyAnimationState_Damege,	// 被弾
+		};
+		// アニメーションステート
+		EnEnemyAnimationState m_enEnemyAnimationState = m_enEnemyAnimationState_Walk;
+
+		// エネミーの行動ステート
+		enum EnEnemyActState
+		{
+			m_enEnemyActState_Craw,			// 巡回
+			m_enEnemyActState_Tracking		// 追跡
+		};
+		// 行動ステート
+		EnEnemyActState m_enEnemyActState = m_enEnemyActState_Craw;
+
+		Vector3 m_position = Vector3::Zero;		// エネミーの座標
+		Vector3 m_forward = Vector3::AxisZ;		// エネミーの前方向
+
+		bool FoundPlayerFlag = false;		// プレイヤーを発見したかどうか
+		bool CatchPlayerFlag = false;		// プレイヤーを捕まえたかどうか
+		bool MissigPlayerFlag = false;		// プレイヤーを見失ったかどうか
+		bool HitFlashBulletFlag = false;	// 閃光弾が当たったかどうか
+		bool ReturnToPassFlag = false;		// 直近のパスに戻ったかどうか
 	};
 
-private:
 
-	ModelRender m_modelRender;			// モデルレンダー
-	FontRender m_fontRender;			// フォントレンダー
+private:
+	ModelRender modelRender;			// モデルレンダー
 
 	// アニメーションクリップ
 	enum EnEnemyAnimationClip
 	{
-		enEnemyAnimationClip_Idle,		// 待機
-		enEnemyAnimationClip_Walk,		// 歩く
-		enEnemyAnimationClip_Run,		// 走る
-		enEnemyAnimationClip_Attack,	// 攻撃
-		enEnemyAnimationClip_Damege,	// 被弾
+		enEnemyAnimationClip_Idle,
+		enEnemyAnimationClip_Walk,
+		enEnemyAnimationClip_Run,
+		enEnemyAnimationClip_Attack,
+		enEnemyAnimationClip_Damege,
 		enEnemyAnimationClip_Num
 	};
 
-	AnimationClip m_animationClips[enEnemyAnimationClip_Num];
-
-	Vector3 m_position;
-
-	bool FoundPlayerFlag = false;		// プレイヤーを発見したかどうか
+	AnimationClip animationClips[enEnemyAnimationClip_Num];
+	
+	bool FoundPlayerFlag = false;		// プレイヤーを見つけたかどうか
+	bool MissingPlayerFlag = false;		// プレイヤーを見失ったかどうか
 	bool CatchPlayerFlag = false;		// プレイヤーを捕まえたかどうか
-	bool MissigPlayerFlag = false;		// プレイヤーを見失ったかどうか
-	bool HitFlashBulletFlag = false;	// 閃光弾が当たったかどうか
-	bool ReturnToPassFlag = false;		// 直近のパスに戻ったかどうか
 };
 
