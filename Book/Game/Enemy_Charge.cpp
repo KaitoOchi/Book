e9269e6c12b7999f752bd9a38c8008246c8a9522
nom0531 <norrrrrra0504@gmail.com>
2023-03-25 14:57:10 +0900

    Enemy_Charge.cppを追加

---
+++ Book/Game/Enemy_Charge.cpp
@@ -0,0 +1,160 @@
+#include "stdafx.h"
+#include "Enemy_Charge.h"
+
+namespace 
+{
+	const float		LINEAR_COMPLETION = 0.2f;		// 線形補完のフレーム数
+	const float		STOP_TIMER = 1.0f;				// 溜め時間
+}
+
+Enemy_Charge::Enemy_Charge()
+{
+}
+
+Enemy_Charge::~Enemy_Charge()
+{
+}
+
+bool Enemy_Charge::Start()
+{
+	// アニメーションの読み込み
+	m_animationClips[m_enAnimationClip_Idle].Load("Assets/animData/enemy/idle.tka");
+	m_animationClips[m_enAnimationClip_Idle].SetLoopFlag(true);
+	m_animationClips[m_enAnimationClip_Walk].Load("Assets/animData/enemy/walk.tka");
+	m_animationClips[m_enAnimationClip_Walk].SetLoopFlag(true);
+	m_animationClips[m_enAnimationClip_Run].Load("Assets/animData/enemy/run.tka");
+	m_animationClips[m_enAnimationClip_Run].SetLoopFlag(true);
+	m_animationClips[m_enAnimationClip_Attack].Load("Assets/animData/enemy/attack1.tka");
+	m_animationClips[m_enAnimationClip_Attack].SetLoopFlag(false);
+	m_animationClips[m_enAnimationClip_Damege].Load("Assets/animData/enemy/damege.tka");
+	m_animationClips[m_enAnimationClip_Damege].SetLoopFlag(false);
+
+	// モデルの読み込み
+	m_ChargeModelRender.Init("Assets/modelData/enemy/enemy.tkm", m_animationClips, m_enAnimationClip_Num, enModelUpAxisZ);
+	m_ChargeModelRender.SetScale(m_scale);
+	m_ChargeModelRender.SetPosition(m_position);
+	m_ChargeModelRender.SetRotation(m_rotation);
+
+	Enemy::Start();
+
+	// 長方形に移動
+	m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z),1 });
+	m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z + 300.0f),2 });
+	m_pointList.push_back({ Vector3(m_position.x + 500.0f ,m_position.y,m_position.z + 300.0f),3 });
+	m_pointList.push_back({ Vector3(m_position.x + 500.0f,m_position.y,m_position.z),4 });
+
+	m_point = &m_pointList[0];
+
+	// 視野を作成
+	Enemy::SpotLight_New(m_position);
+
+	return true;
+}
+
+void Enemy_Charge::Update()
+{
+	Act();			// 行動パターン
+	Animation();	// アニメーション
+
+	// 更新
+	m_ChargeModelRender.SetScale(m_scale);
+	m_ChargeModelRender.SetPosition(m_position);
+	m_characterController.SetPosition(m_position);
+
+	// キャラクターコントローラーをモデルの位置と同期
+	Vector3 move = Vector3::Zero;
+	m_position = m_characterController.Execute(move, g_gameTime->GetFrameDeltaTime());
+
+	m_ChargeModelRender.Update();
+}
+
+void Enemy_Charge::Act()
+{
+	Enemy::HitFlashBullet();		// 閃光弾に当たったときの処理
+
+	// スポットライト
+	Enemy::SpotLight_Serch(m_rotation, m_position);
+
+	// プレイヤーを発見したとき
+	if (Enemy::SeachPlayer() == true) {
+		Enemy::Act_Access();	// 突進攻撃
+
+		if (Enemy::SeachPlayer() == false) {
+			Enemy::Act_Craw();		// 巡回
+		}
+	}
+	else {
+		Enemy::Act_Craw();			// 巡回
+	}
+}
+
+void Enemy_Charge::Pass(int PassState)
+{
+	switch (PassState)
+	{
+		// 縦
+	case Line:
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z),1 });
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z - 500.0f),2 });
+		break;
+		// 横
+	case Horizontal:
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z),1 });
+		m_pointList.push_back({ Vector3(m_position.x + 500.0f,m_position.y,m_position.z),2 });
+		break;
+		// 右回り
+	case RightRotation:
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z),1 });
+		m_pointList.push_back({ Vector3(m_position.x + 500.0f,m_position.y,m_position.z),2 });
+		m_pointList.push_back({ Vector3(m_position.x + 500.0f,m_position.y,m_position.z - 500.0f),3 });
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z - 500.0f),4 });
+		break;
+		// 左回り
+	case LeftRotation:
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z),1 });
+		m_pointList.push_back({ Vector3(m_position.x - 500.0f,m_position.y,m_position.z),2 });
+		m_pointList.push_back({ Vector3(m_position.x - 500.0f,m_position.y,m_position.z - 500.0f),3 });
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z - 500.0f),4 });
+		break;
+		// (左に)直角
+	case RightAngle:
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z),1 });
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z - 500.0f),2 });
+		m_pointList.push_back({ Vector3(m_position.x - 500.0f,m_position.y,m_position.z - 500.0f),3 });
+		m_pointList.push_back({ Vector3(m_position.x,m_position.y,m_position.z + 500.0f),4 });
+		break;
+	}
+}
+
+void Enemy_Charge::Animation()
+{
+	// アニメーションのステート
+	switch (m_enEnemyAnimationState) {
+		// 待機
+	case Enemy::m_enEnemyAnimationState_Idle:
+		m_ChargeModelRender.PlayAnimation(m_enAnimationClip_Idle, LINEAR_COMPLETION);
+		break;
+		// 歩く
+	case Enemy::m_enEnemyAnimationState_Walk:
+		m_ChargeModelRender.PlayAnimation(m_enAnimationClip_Walk, LINEAR_COMPLETION);
+		break;
+		// 走る
+	case Enemy::m_enEnemyAnimationState_Run:
+		m_ChargeModelRender.PlayAnimation(m_enAnimationClip_Run, LINEAR_COMPLETION);
+		break;
+		// 攻撃
+	case Enemy::m_enEnemyAnimationState_Attack:
+		m_ChargeModelRender.PlayAnimation(m_enAnimationClip_Attack, LINEAR_COMPLETION);
+		break;
+		// 被弾
+	case Enemy::m_enEnemyAnimationState_Damege:
+		m_ChargeModelRender.PlayAnimation(m_enAnimationClip_Damege, LINEAR_COMPLETION);
+		break;
+	}
+}
+
+void Enemy_Charge::Render(RenderContext& rc)
+{
+	// 描画
+	m_ChargeModelRender.Draw(rc);
+}
