c66187e3760bb938b45b0224a5808fb47f9800b9
nom0531 <norrrrrra0504@gmail.com>
2023-04-30 08:22:45 +0900

    wallクラスの更新を行いました。モデルごとにクラスを追加

---
+++ Book/Game/Stage/Wall/Wall_Gap.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "Wall_Gap.h"
+
+Wall_Gap::Wall_Gap()
+{
+
+}
+
+Wall_Gap::~Wall_Gap()
+{
+
+}
+
+bool Wall_Gap::Start()
+{
+	// ƒ‚ƒfƒ‹‚ğ“Ç‚İ‚Ş
+	m_wallRender.Init("Assets/modelData/level_test/tkm/wall1.tkm", 0, 0, enModelUpAxisZ, 1, 1);
+
+	Wall::Start();
+
+	return true;
+}
+
+void Wall_Gap::Render(RenderContext& rc)
+{
+	m_wallRender.Draw(rc);
+}
