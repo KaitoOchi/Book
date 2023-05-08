5f6c00494fea78f529f7dad0ee89d220e145cadd
CA01972058 <CA01972058@st.kawahara.ac.jp>
2023-05-08 15:05:32 +0900

    SkyCube実装ver1.01

--- Book/BookEngine/SkyCube.cpp
+++ Book/BookEngine/SkyCube.cpp
@@ -16,7 +16,7 @@ namespace nsBookEngine {
 	bool SkyCube::Start()
 	{
 		//テクスチャの初期化
-		m_texture.InitFromDDSFile(L"Assets/modelData/preset/skyCubeMap.dds");
+		m_texture.InitFromDDSFile(L"Assets/modelData/preset/skyCubeMapNight.dds");
 
 		//モデルの初期化
 		ModelInitData initData;
@@ -30,6 +30,11 @@ namespace nsBookEngine {
 		initData.m_expandConstantBuffer = &m_luminance;
 		initData.m_expandConstantBufferSize = sizeof(m_luminance);
 		initData.m_colorBufferFormat[0] = DXGI_FORMAT_R16G16B16A16_FLOAT;
+
+		Quaternion rot;
+		rot.AddRotationX(Math::DegToRad(-90.0f));
+
+		m_modelRender.SetRotation(rot);
 
 		m_modelRender.InitModelData(initData);
 		m_modelRender.SetPosition(m_position);
