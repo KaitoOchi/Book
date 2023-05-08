80b6aef2aa2dcdd336ac8a89dcb077840728a355
CA01972044 <CA01972044@st.kawahara.ac.jp>
2023-05-08 15:02:20 +0900

    tyouika

--- Book/Game/Stage/GhostBox.cpp
+++ Book/Game/Stage/GhostBox.cpp
@@ -12,7 +12,7 @@ GhostBox::~GhostBox()
 bool GhostBox::Start()
 {
 	Ghost::Start();
-	m_boxobje.Create(Vector3(m_boxSize), m_position, m_rotation);
+	m_boxobje.Create(m_boxSize, m_position, m_rotation);
 	return true;
 
 }
