6ca0a5562a320ae7c9b4f72c68bacf9c1dc22294
nom0531 <norrrrrra0504@gmail.com>
2023-05-15 18:18:36 +0900

    wall_Gap内の使用していない処理を削除しました

--- Book/Game/Stage/Wall/Wall_Gap.cpp
+++ Book/Game/Stage/Wall/Wall_Gap.cpp
@@ -21,25 +21,6 @@ bool Wall_Gap::Start()
 	return true;
 }
 
-void Wall_Gap::ModelLoad(int i)
-{
-	//switch (i) {
-	//case 1:
-	//	// ƒ‚ƒfƒ‹‚ğ“Ç‚İ‚Ş
-	//	m_wallRender.Init("Assets/modelData/level_test/tkm/wall_gap.tkm", 0, 0, enModelUpAxisZ, true, true, 0);
-	//	break;
-	//case 2:
-	//	// ƒ‚ƒfƒ‹‚ğ“Ç‚İ‚Ş
-	//	m_wallRender.Init("Assets/modelData/level_test/tkm/wall2.tkm", 0, 0, enModelUpAxisZ, true, true, 0);
-	//	break;
-	//case 3:
-
-	//	// ƒ‚ƒfƒ‹‚ğ“Ç‚İ‚Ş
-	//	m_wallRender.Init("Assets/modelData/level_test/tkm/wall3.tkm", 0, 0, enModelUpAxisZ, true, true, 0);
-	//	break;
-	//}
-}
-
 void Wall_Gap::Render(RenderContext& rc)
 {
 	Wall::Render(rc);
