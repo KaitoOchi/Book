284c80bbdbda5d0382ff4804695a12582faf3be4
CA01972058 <CA01972058@st.kawahara.ac.jp>
2023-05-22 16:39:58 +0900

    プレイヤーとエネミーが半透明の部分を修正

--- Book/Game/UI/MiniMap.h
+++ Book/Game/UI/MiniMap.h
@@ -1,19 +1,15 @@
 #pragma once
 
 class PlayerManagement;
-class Enemy_Normal;
-class Enemy_Search;
-class Enemy_Charge;
-class Game;
 class Enemy;
-class Treasure;
 class PhysicsGhost;
 
 using namespace std;
+
 namespace
 {
-	const int ENEMY_NUM = 50;			// 表示するかどうかのフラグ用。多めに用意してます
-	const int WALL_NUM = 10;
+	const int ENEMY_NUM = 50;	//敵の数。少し多めに
+	const int WALL_NUM = 10;	//隙間の数。少し多めに
 }
 
 class MiniMap:public IGameObject
@@ -36,20 +32,20 @@ public:
 		m_treasurePos = pos;
 
 		if (color) {
-			m_TreasureSpriteRender.SetMulColor({ 1.0f,0.0f,0.0f,1.0f });
+			m_treasureSpriteRender.SetMulColor({ 1.0f,0.0f,0.0f,1.0f });
 		}
 	}
 
 private:
 	/// <summary>
-	/// 描画処理。
+	/// 描画共通処理。
 	/// </summary>
 	bool DrawMap(const Vector3& mapPos, float& alpha);
 
 	/// <summary>
-	/// 敵を描画する処理。
+	/// アクタを描画する処理。
 	/// </summary>
-	void DrawMap_Enemy();		
+	void DrawMap_Actor();		
 
 	/// <summary>
 	/// ワールド座標系からマップ座標系に変換。
@@ -58,31 +54,25 @@ private:
 	/// <param name="worldPosition">マップに表示したいオブジェクトのワールド座標</param>
 	/// <returns></returns>
 	const bool WorldPositionConvertToMapPosition(
-		Vector3 worldcenterPosition,
-		Vector3 worldPosition
+		const Vector3& worldcenterPosition,
+		const Vector3& worldPosition
 	);
 
 private:
-	SpriteRender						m_SpriteRender;							// スプライトレンダー。ミニマップのベース
-	SpriteRender						m_OutLineSpriteRender;						// スプライトレンダー。ミニマップの装飾部分
-	SpriteRender						m_PlayerSpriteRender;						// スプライトレンダー。プレイヤー
-	SpriteRender						m_TreasureSpriteRender;					// スプライトレンダー。お宝
-	array< SpriteRender, ENEMY_NUM >	m_EnemySpriteRender;	// スプライトレンダー。エネミー
-	array< SpriteRender, WALL_NUM >		m_wallSpriteRender;//スプライトレンダー。壁
-
-	PlayerManagement*					m_playerManagement = nullptr;
-	Game*								m_game = nullptr;
-
-	vector<Enemy*>						m_enemyList;						// エネミーのリスト
-	vector<PhysicsGhost*>				m_physicsGhostList;	//壁のリスト
-
-	Vector3								m_treasurePos;			// お宝の位置
-	Vector3								m_playerPos;							//マップ上のプレイヤーの座標
-	Vector3								m_mapPos;								//計算したマップに乗せる対象の座標
-
-	std::array< bool, ENEMY_NUM >m_isImage;						// 表示するかどうかのフラグ。エネミーの数分用意する
-	std::array< bool, WALL_NUM > m_enableWallSprite;
-
-	bool m_isTreasure = false;	//お宝かどうか
+	SpriteRender						m_spriteRender;					//ミニマップの背景画像
+	SpriteRender						m_outlineSpriteRender;			//ミニマップの装飾画像
+	SpriteRender						m_playerSpriteRender;			//プレイヤー画像
+	SpriteRender						m_treasureSpriteRender;			//お宝画像
+	array< SpriteRender, ENEMY_NUM >	m_enemySpriteRender;			//敵画像の配列
+	array< SpriteRender, WALL_NUM >		m_wallSpriteRender;				//壁画像の配列
+	PlayerManagement*					m_playerManagement = nullptr;	//プレイヤーマネジメントクラス
+	vector<Enemy*>						m_enemyList;					//敵のリスト
+	vector<PhysicsGhost*>				m_physicsGhostList;				//壁のリスト
+	Vector3								m_treasurePos;					//お宝の位置
+	Vector3								m_playerPos;					//マップ上のプレイヤーの座標
+	Vector3								m_mapPos;						//計算したマップに乗せる対象の座標
+	bool								m_isTreasure = false;			//お宝かどうか
+	array< bool, ENEMY_NUM >			m_enableEnemySprites;			//敵を表示するかどうかのフラグ。敵の数分用意する
+	array< bool, WALL_NUM >				m_enableWallSprites;			//壁を表示するかどうかのフラグ。壁の数分用意する
 };
 
