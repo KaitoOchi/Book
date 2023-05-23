d071c93b690fca418553c97959995e25051ed9ce
CA01972058 <CA01972058@st.kawahara.ac.jp>
2023-05-23 16:11:15 +0900

    修正したって言ってんだろ”！！！！！！！！！！！！！！！！！！！！

--- Book/Game/Scene/Title_Guide.cpp
+++ Book/Game/Scene/Title_Guide.cpp
@@ -6,10 +6,10 @@
 
 namespace
 {
-	const Vector3 BEZIER_POS[4] = { Vector3(-25.0f, 25.0f, 0.0f),
-									Vector3(25.0f, 25.0f, 0.0f), 
-									Vector3(25.0f, -25.0f, 0.0f),
-									Vector3(-25.0f, -25.0f, 0.0f) };		//ベジェ曲線の座標
+	const Vector3 BEZIER_POS[4] = { Vector3(-25.0f, -25.0f, 0.0f),
+									Vector3(125.0f, -25.0f, 0.0f), 
+									Vector3(25.0f, -50.0f, 0.0f),
+									Vector3(-125.0f, -50.0f, 0.0f) };		//ベジェ曲線の座標
 }
 
 Title_Guide::Title_Guide()
@@ -29,22 +29,23 @@ Title_Guide::~Title_Guide()
 bool Title_Guide::Start()
 {
 	//ガイド背景画面の設定
-	m_guideBackSpriteRender.Init("Assets/sprite/UI/guide/guide_all2.DDS", 1920.0f, 1080.0f);
+	m_guideBackSpriteRender.Init("Assets/sprite/UI/guide/guide.DDS", 1157.0f, 152.0f);
+	m_guideBackSpriteRender.SetPosition(Vector3(-250.0f, 415.0f, 0.0f));
 	m_guideBackSpriteRender.Update();
 	m_sprites.push_back(&m_guideBackSpriteRender);
 
 	//ガイド画面の設定
-	m_guideSpriteRender[0].Init("Assets/sprite/UI/guide/guide_playguide.DDS", 1403.0f, 637.0f);
-	m_guideSpriteRender[1].Init("Assets/sprite/UI/guide/rule.DDS", 1141.0f, 541.0f);
+	m_guideSpriteRender[0].Init("Assets/sprite/UI/guide/guide_playguide.DDS", 1178.0f, 755.0f);
+	m_guideSpriteRender[1].Init("Assets/sprite/UI/guide/rule.DDS", 1178.0f, 755.0f);
 	for (int i = 0; i < 2; i++) {
-		m_guideSpriteRender[i].SetPosition(BEZIER_POS[i * 2]);
+		m_guideSpriteRender[i].SetPosition(BEZIER_POS[2 - (i * 2)]);
 		m_guideSpriteRender[i].Update();
 		m_sprites.push_back(&m_guideSpriteRender[i]);
 	}
 
 	//ボタン画像の設定
-	m_buttonSpriteRender[0].Init("Assets/sprite/UI/button/text_Abutton.DDS", 287.0f, 152.0f);
-	m_buttonSpriteRender[1].Init("Assets/sprite/UI/button/text_Bbutton.DDS", 287.0f, 152.0f);
+	m_buttonSpriteRender[0].Init("Assets/sprite/UI/button/text_Bbutton.DDS", 287.0f, 152.0f);
+	m_buttonSpriteRender[1].Init("Assets/sprite/UI/button/text_crosskey.DDS", 287.0f, 152.0f);
 	for (int i = 0; i < 2; i++) {
 		m_buttonSpriteRender[i].SetPosition(Vector3(-825.0f, -375.0f - (i * 50.0f), 0.0f));
 		m_buttonSpriteRender[i].SetPivot(Vector2(0.0f, 0.5f));
@@ -73,31 +74,48 @@ void Title_Guide::Update()
 
 void Title_Guide::Input()
 {
-	//Aボタンが押されたら
-	if (g_pad[0]->IsTrigger(enButtonA))
+	//右ボタンが押されたら
+	if (g_pad[0]->IsTrigger(enButtonRight))
 	{
-		if (m_cursor != 0) {
-			m_isSceneChange = true;
-		}
-		else {
-			m_cursor++;
-		}
-		m_timer = 1.0f;
+		m_cursor++;
 		m_isWaitState = true;
 		//可能ならSEを再生
 		m_title->IsCanPlaySound(true);
 
+		if (m_cursor == 1) {
+			m_timer = 1.0f;
+		}
+		else {
+			m_timer = 0.0f;
+		}
+
+		if (m_cursor > 1) {
+			m_cursor = 0;
+		}
+
+	}
+	//左ボタンが押されたら
+	else if (g_pad[0]->IsTrigger(enButtonLeft)) {
+		m_cursor--;
+		m_isWaitState = true;
+		//可能ならSEを再生
+		m_title->IsCanPlaySound(true);
+
+		if (m_cursor == 1) {
+			m_timer = 1.0f;
+		}
+		else {
+			m_timer = 0.0f;
+		}
+
+		if (m_cursor < 0) {
+			m_cursor = 1;
+		}
 	}
 	//Bボタンが押されたら
 	else if (g_pad[0]->IsTrigger(enButtonB))
 	{
-		if (m_cursor == 1) {
-			m_cursor = 0;
-		}
-		else {
-			m_isSceneChange = true;
-
-		}
+		m_isSceneChange = true;
 		m_timer = 1.0f;
 		m_isWaitState = true;
 		//可能ならSEを再生
@@ -119,7 +137,7 @@ void Title_Guide::StateChange()
 	}
 	else {
 		//アニメーションが終了したら
-		if (m_timer < 0.0f) {
+		if (m_timer < 0.0f || m_timer > 1.0f) {
 
 			if (m_isSceneChange) {
 				m_isWaitFadeOut = true;
@@ -131,7 +149,12 @@ void Title_Guide::StateChange()
 			}
 		}
 		else {
-			m_timer -= g_gameTime->GetFrameDeltaTime();
+			if (m_isSceneChange || m_cursor == 1) {
+				m_timer -= g_gameTime->GetFrameDeltaTime();
+			}
+			else {
+				m_timer += g_gameTime->GetFrameDeltaTime();
+			}
 		}
 	}
 
@@ -173,16 +196,13 @@ void Title_Guide::Animation()
 	}
 
 	//画像の全面を反転させる
-	if (m_timer < 0.5f && m_timer > 0.49f) {
+	if (m_timer < 0.5f && m_timer > 0.48f) {
 		m_forward = !m_forward;
 	}
 }
 
 void Title_Guide::Render(RenderContext& rc)
 {
-	//背景画像の描画
-	m_guideBackSpriteRender.Draw(rc);
-
 	//ガイド画像の描画
 	if (m_forward) {
 		m_guideSpriteRender[0].Draw(rc);
@@ -198,4 +218,6 @@ void Title_Guide::Render(RenderContext& rc)
 		m_buttonSpriteRender[i].Draw(rc);
 	}
 
+	//背景画像の描画
+	m_guideBackSpriteRender.Draw(rc);
 }
