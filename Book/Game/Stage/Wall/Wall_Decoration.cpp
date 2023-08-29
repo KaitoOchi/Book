d616a8f11457d5ec80fa2e6666c248b18d118b38
nom0531 <norrrrrra0504@gmail.com>
2023-08-29 14:42:41 +0900

    檻のモデルを実装しました。イベント開始時に削除されるようにしています

--- Book/Game/Stage/Wall/Wall_Decoration.cpp
+++ Book/Game/Stage/Wall/Wall_Decoration.cpp
@@ -14,10 +14,22 @@ Wall_Decoration::~Wall_Decoration()
 bool Wall_Decoration::Start()
 {
 	Wall::Start();
+
+	if (!m_isCreate) {
+		return true;
+	}
+
+	m_physicsStaticObj.CreateFromModel(m_wallRender.GetModel(), m_wallRender.GetModel().GetWorldMatrix());
+
 	return true;
 }
 
 void Wall_Decoration::Render(RenderContext& rc)
 {
-	Wall::Render(rc);
+	if (!m_isCreate) {
+		Wall::Render(rc);
+	}
+	else {
+		m_wallRender.Draw(rc);
+	}
 }
